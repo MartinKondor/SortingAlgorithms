02818f9494a5a5618fcfc151bf417bf889f9e893
Kondor Martin <martinkondor@MiMac.local>
2021-05-12 22:17:04 +0200

    First commit

---
+++ README.md
@@ -0,0 +1,33 @@
+<p align="center"><img src="public/img/SortingAlgorithms.gif" width="450"></p>
+
+# 📊 SortingAlgorithms
+
+[![Project Status](https://img.shields.io/badge/status-active-brightgreen.svg)](https://github.com/MartinKondor/SortingAlgorithms/)
+[![version](https://img.shields.io/badge/version-2019.07-brightgreen.svg)](https://github.com/MartinKondor/SortingAlgorithms/)
+[![GitHub Issues](https://img.shields.io/github/issues/MartinKondor/SortingAlgorithms.svg)](https://github.com/MartinKondor/SortingAlgorithms/issues)
+![Contributions welcome](https://img.shields.io/badge/contributions-welcome-blue.svg)
+[![License](https://img.shields.io/badge/license-MIT-blue.svg)](https://opensource.org/licenses/MIT)
+
+Sorting algorithms visualized.
+
+## Contributing
+
+This project is open for any kind of contribution from anyone.
+
+### Steps
+
+1. Fork this repository
+2. Create a new branch (optional)
+3. Clone it
+4. Make your changes
+5. Upload them
+6. Make a pull request here
+7. Profit.
+
+## Authors
+
+* **[Martin Kondor](https://github.com/MartinKondor)**
+
+# License
+
+See the [LICENSE](LICENSE) file for details.
---
+++ public/js/index.js
@@ -0,0 +1,157 @@
+(function ($) {
+    // https://en.wikipedia.org/wiki/X_%2B_Y_sorting
+    const CANVAS = document.getElementById("myCanvas");
+    const CTX = CANVAS.getContext("2d");
+    const screenWidth = 1200, screenHeight = 400;
+    let towerWidth = 40;
+    let towers = [];
+
+    CTX.lineWidth = 1;
+
+
+    class Tower {
+        constructor(xpos, ypos, width, height) {
+            this.xpos = xpos;
+            this.ypos = ypos;
+            this.width = width;
+            this.height = height;
+        }
+        
+        draw() {
+            CTX.fillStyle = "#000000";
+            CTX.beginPath();
+            CTX.fillRect(this.xpos, this.ypos, this.width, this.height);
+            CTX.stroke();   
+        }
+        
+        erase() {
+            CTX.fillStyle = "#FAFAFA";
+            CTX.beginPath();
+            CTX.fillRect(this.xpos, this.ypos, this.width, screenHeight);
+            CTX.stroke();  
+        }
+            
+        setHeight(h) {
+            this.erase();
+            this.height = h;
+            this.draw();
+        }
+        
+        update(xpos, ypos) {
+
+            // Remove the previous tower
+            CTX.fillStyle = "#FAFAFA";
+            CTX.beginPath();
+            CTX.fillRect(this.xpos, this.ypos, this.width, 400);
+            CTX.stroke();
+            
+            // Draw the new one
+            this.xpos = xpos;
+            this.ypos = ypos;
+            
+            CTX.fillStyle = "#000000";
+            CTX.beginPath();
+            CTX.fillRect(this.xpos, this.ypos, this.width, this.height);
+            CTX.stroke();     
+        }
+    }
+
+    function createTowers() {
+        towers = [];
+        let N = screenWidth / towerWidth - 1;
+        
+        for (let i = 0; i < N; i++) {
+            let h = Math.floor(Math.random() * (screenHeight - 1)) + 1;
+            // let h = i * (screenHeight - 10) / N + 10;
+            towers.push(new Tower(i + i*towerWidth, screenHeight - h, towerWidth, h));
+        }
+    }
+
+    function eraseTowers() {
+        for (let i = 0; i < towers.length; i++) {
+            towers[i].erase();
+        }
+    }
+
+    function drawTowers() {
+        for (let i = 0; i < towers.length; i++) {
+            towers[i].draw();
+        } 
+    }
+
+    function swapTowers(i1, i2) {
+        towers[i1].erase();
+        towers[i2].erase();
+
+        let temp = towers[i1].xpos;
+        towers[i1].xpos = towers[i2].xpos;
+        towers[i2].xpos = temp;
+
+        towers[i1].draw();
+        towers[i2].draw();
+
+        temp = towers[i1];
+        towers[i1] = towers[i2];
+        towers[i2] = temp;
+    }
+
+    // Sorting algorithms
+    function bubbleSort(delay) {
+        let i = 0;
+        let swapped = false;
+
+        let interval = setInterval(function () {
+            if (towers[i + 1].height < towers[i].height) {
+                swapTowers(i, i + 1);
+                swapped = true;
+            }
+
+            if (++i == towers.length - 1) {
+                if (!swapped) {
+                    clearInterval(interval);
+                    $('#randomize').prop('disabled', false);
+                    $('#start').prop('disabled', false);
+                }
+
+                i = 0;
+                swapped = false;
+            }           
+        }, delay);
+    }
+
+    createTowers();
+    drawTowers();
+    
+    let lastSortFunction = bubbleSort;
+    let delayTime = 99;
+    $('#speedSlider').val(delayTime);
+
+    $('#start').on('click', function () {
+        $('#randomize').prop('disabled', true);
+        $('#start').prop('disabled', true);
+
+        // Get speed data if not changed
+        delayTime = 100 - $('#speedSlider').val();
+        
+        lastSortFunction(delayTime);
+    });
+
+    $('#randomize').on('click', function () {
+        eraseTowers();
+        createTowers();
+        drawTowers();
+    });
+
+    $('#speedSlider').on('change', function () {
+        delayTime = 100 - $('#speedSlider').val();
+    });
+
+    $('#nOfTowers').on('change', function () {
+        eraseTowers();
+        towerWidth = 40 - $('#nOfTowers').val() + 1;
+        createTowers();
+        drawTowers();
+    });
+
+})(jQuery);
+
